5290ff21c7fee5747c67c0c89b46d1a2390676a9
LeoPinzano <lucatherogue@gmail.com>
2024-09-19 12:31:44 +0200

    Modif main

--- cryptoCielTest.cpp
+++ cryptoCielTest.cpp
@@ -13,22 +13,27 @@ int main()
 	std::cout << LM.CalculateFileSHA256(filename) << std::endl;
 
 	// Test AES
+	/*
 	AesGestion aesGestion;
 	aesGestion.GenerateAESKey();
 	aesGestion.SaveAESKeyToFile("../Keys/AES/key.txt");
 	aesGestion.EncryptFileAES256(filename, "../DocChiffreAES.pem");
 	aesGestion.DecryptFileAES256("../DocChiffreAES.pem", "../DocDechiffreAES.txt");
+	*/
 
 	// Test RSA
 	RsaGestion rsaGestion;
-	
+	/*
 	std::string message = "Un super message super chouette";
 	rsaGestion.generationClef("../Keys/RSA/Public/publicKey.txt", "../Keys/RSA/Private/privateKey.txt", 2048);
 	rsaGestion.dechiffrementRsa(rsaGestion.chiffrementRsa(message));
+	*/
 	
+	rsaGestion.chargementClefs("../Keys/RSA/Public/publicKey.txt", "../Keys/RSA/Private/privateKey.txt");
+	//rsaGestion.chiffrementFichier(filename, "../DocChiffreRSA.pem", true);
+	rsaGestion.dechiffreFichier("../donnees_chiffrees.txt");
 	/*
 	rsaGestion.chargementClefs("../Keys/RSA/Public/publicKey.txt", "../Keys/RSA/Private/privateKey.txt");
-	rsaGestion.chiffrementFichier(filename, "../DocChiffreRSA.pem", true);
-	rsaGestion.dechiffreFichier(".. / DocChiffreRSA.pem");
+	rsaGestion.chiffrementFichier("../Keys/AES/key.txt", "../CleChiffreRSA.pem", true);
 	*/
 }
